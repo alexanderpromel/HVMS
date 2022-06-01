86de68a885a07bf754dd64cf7d341c4ea7bb1610
alexanderpromel <alexanderpromel@gmail.com>
2022-06-01 13:37:55 +0530

    task completed

--- src/org/firstfloor/A_Section.java
+++ src/org/firstfloor/A_Section.java
@@ -11,12 +11,15 @@ public class A_Section extends B_Section {
 	public void thridStudent() {
 		System.out.println("Prithvi");
 	}
-	
+	public void vehicleName() {
+		System.out.println("Mercedez benz");
+	}
 	public static void main(String[]args) {
 		A_Section a = new A_Section();
 		a.firstStudent();
 		a.secondStudent();
 		a.thridStudent();
+		a.vehicleName();
 	}
 
 }
